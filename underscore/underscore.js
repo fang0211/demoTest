693f06adbc9e1222f238cd1933f23532a3d2478d
fang0211 <962351444@qq.com>
2019-12-20 16:55:25 +0800

    --增加

--- underscore/underscore.js
+++ underscore/underscore.js
@@ -259,6 +259,41 @@
     _.shuffle = function (array) {
         return _.sample(array,Infinity)
     }
+    _.isArguments = function (array) {
+
+    }
+    flatten = function(array,shallow){
+        var ret = [];
+        var index = 0;
+        for(var i =0;i<array.length;i++){
+            var value = array[i];
+            if(_.isArray(value) || _.isArguments(value)){
+                if(!shallow){
+                    value = flatten(value,shallow)
+                }
+                var j=0;
+                ret.length += value.length
+                while(j<value.length){
+                    ret[index++] = value[j++]
+                }
+            }else{
+                ret[index++] = value
+            }
+        }
+        return ret;
+
+    }
+    _.flatten = function (array,shallow) {
+        return flatten(array,shallow)
+    }
+    //返回数组中除了最后一个元素外的其他全部元素。 在arguments对象上特别有用。
+    _.initial = function(array,n){
+        return [].slice.call(array,0,Math.max(0,array.length-(n==null?1:n)))
+    }
+    //返回数组中除了第一个元素外的其他全部元素。传递 n 参数将返回从n开始的剩余所有元素
+    _.rest = function (array,n) {
+        return [].slice.call(array,(n==null ? 1: n))
+    }
     _.mixin = function(obj){
         _.each(_.function(obj),function(name){
             var func = obj[name];
